c9275f5a249b39a0e2eb04a52077057a33162874
Olivier Van Goethem <olivier.van-goethem@cloudreach.co.uk>
2017-06-07 20:38:23 +0100

    Refactor

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
 from flask import Flask, render_template
-import boto3
-from collections import defaultdict
-import json
+
+from api_client import BigBotherAPIClient
 
 app = Flask(__name__)
+api = BigBotherAPIClient()
+
 
 @app.route('/', methods=['GET', 'POST'])
-def lambda_handler(event=None, context=None):
-    return render_template('search.html')
+def index():
+    people = api.list_people()
+    return render_template('index.html', people=people)
 
-# @app.route('/search', methods=['GET', 'POST'])
-# def search():
-#     if request.method == 'POST' and form.validate_on_submit():
-#         return redirect((url_for('search_results', query=form.search.data)))  # or what you want
-#     return render_template('search.html', form=form)
-
-# def find_person():
-#     # lookup user in index
 
 if __name__ == '__main__':
     app.run(debug=True)
